2136b1f9a45271ba569af2cfc7816cdbac4832df
minghui <847800365@qq.com>
2017-01-11 23:02:34 +0800

    第一次提交演示

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 辉哥 on 2017/1/11.
+ */
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
+
+
